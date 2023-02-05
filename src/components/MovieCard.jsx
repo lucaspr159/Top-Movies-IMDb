2c80537004b25133db5ce04663c122fe0dc58801
Lucas P. Rodrigues <110637825+lucaspr159@users.noreply.github.com>
2023-02-05 19:48:30 -0300

    React finalizado

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,7 +9,7 @@ const MovieCard = ({movie, showLink = true}) => {
         <img src={imageUrl + movie.poster_path} alt={movie.title} />
         <h2>{movie.title}</h2>
         <p>
-            <FaStar/>{movie.vote_average}
+            <FaStar color='#f7d354'/>{movie.vote_average}
         </p>
         {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
     </div>
